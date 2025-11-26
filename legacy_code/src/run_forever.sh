4a73914fc5dbf62315ecf007c5a3795ff709ad72
SpaceMasterCarlsen <rasmuscarlsen55@gmail.com>
2025-11-26 16:09:37 +0100

    project better structure

---
+++ legacy_code/src/run_forever.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+PYTHON_SCRIPT_PATH=$1
+
+
+while true
+do
+    
+    
+    if ! python "$PYTHON_SCRIPT_PATH"; then
+        exit_code=$?
+        echo "Script crashed with exit code $exit_code. Restarting..." >&2
+        sleep 1
+    fi
+done
