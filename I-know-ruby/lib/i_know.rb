72b3f05dfd73e5a36d053baa1d8e919cc0978dfe
SpaceMasterCarlsen <rasmuscarlsen55@gmail.com>
2025-12-04 01:37:47 +0100

    have fully linted the whole project with rubocop, now all is perfect there. Next will be using reek to lint the smells!

--- I-know-ruby/lib/i_know.rb
+++ I-know-ruby/lib/i_know.rb
@@ -1,6 +1,8 @@
-#lib/i_know.rb
+# frozen_string_literal: true
+
+# lib/i_know.rb
 module IKnow
 end
 
-require "dotenv/load"
-require_relative "i_know/app"
+require 'dotenv/load'
+require_relative 'i_know/app'
